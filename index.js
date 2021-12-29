d66d7d4c0c6b1dca41415d5acdb40e73b4914a13
RedCrafter07 <owner@redcrafter07.de>
2021-12-29 19:15:35 +0100

    Simplified stuff

--- index.js
+++ index.js
@@ -17,48 +17,42 @@ module.exports = class {
 		return this;
 	}
 
-	async setup() {
-		return {
-			id: this.id,
-			token: this.token,
-			async getUser(secret) {
-				let returnValue;
-				await axios
-					.post('https://auth.redcrafter07.de/api/getToken', {
-						headers: {
-							Authorization: this.token,
-							ID: this.id,
-							token: secret
-						}
-					})
-					.then(res => (returnValue = res.data))
-					.catch(e =>
-						console.log(
-							chalk.red(`[RE-AUTH-API]: API Error: ${e.response.status} | ${e.response.statusText}`),
-							e.response
-						)
-					);
+	async getUser(secret) {
+		let returnValue;
+		await axios
+			.post('https://auth.redcrafter07.de/api/getToken', {
+				headers: {
+					Authorization: this.token,
+					ID: this.id,
+					token: secret
+				}
+			})
+			.then(res => (returnValue = res.data))
+			.catch(e =>
+				console.log(
+					chalk.red(
+						`[RE-AUTH-API]: API Error: ${e.response.status} | ${e.response.statusText} | ${e.response.data}`
+					)
+				)
+			);
 
-				return returnValue;
-			},
-			async validate() {
-				let returnValue;
-				await axios
-					.get('https://auth.redcrafter07.de/api', {
-						headers: {
-							Authorization: this.token,
-							ID: this.id
-						}
-					})
-					.then(res => (returnValue = res.data))
-					.catch(e =>
-						console.log(
-							chalk.red(`[RE-AUTH-API]: API Error: ${e.response.status} | ${e.response.statusText}`)
-						)
-					);
+		return returnValue;
+	}
 
-				return returnValue;
-			}
-		};
+	async validate() {
+		let returnValue;
+		await axios
+			.get('https://auth.redcrafter07.de/api', {
+				headers: {
+					Authorization: this.token,
+					ID: this.id
+				}
+			})
+			.then(res => (returnValue = res.data))
+			.catch(e =>
+				console.log(chalk.red(`[RE-AUTH-API]: API Error: ${e.response.status} | ${e.response.statusText}`))
+			);
+
+		return returnValue;
 	}
 };
